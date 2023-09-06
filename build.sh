1eb6b51d1382455b6f8a147dc330c12bde7cd81a
liyuanbiao <1834817304@qq.com>
2023-09-07 01:16:52 +0800

    优化构建参数

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker buildx build --tag liyuanbiao/sing-box:latest --tag liyuanbiao/sing-box:1.5.0-beta.3 --push --platform linux/amd64,linux/amd64/v3,linux/arm64,linux/arm/v7,linux/s390x .
+docker buildx build --tag liyuanbiao/sing-box:latest --tag liyuanbiao/sing-box:$1 --build-arg SING_BOX_VERSION=$1 --push --platform linux/amd64,linux/amd64/v3,linux/arm64,linux/arm/v7,linux/s390x .
